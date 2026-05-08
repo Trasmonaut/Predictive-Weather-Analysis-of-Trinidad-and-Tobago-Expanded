6140332b2e983ce7c5d82e908c425487462bbb10
Caleb <c.ramdath.26@gmail.com>
2026-05-08 18:54:52 -0400

    Rebound port number for Flask app to avoid conflict with another service.

--- Webpage/app.py
+++ Webpage/app.py
@@ -364,5 +364,5 @@ def create_app(config_object=Config):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5100)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
