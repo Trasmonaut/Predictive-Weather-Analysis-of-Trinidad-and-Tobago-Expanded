d0853dacea7ca0474ad189b19b398f18375c84ff
Caleb <c.ramdath.26@gmail.com>
2026-05-08 19:18:21 -0400

    Updated for Render Deployment

--- Webpage/app.py
+++ Webpage/app.py
@@ -361,5 +361,11 @@ def create_app(config_object=Config):
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=5000
+    )
